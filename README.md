6e803f4c225a8d2aacdd3b95dfa88af314211295
Martin Silva <martins80@hotmail.es>
2023-02-12 21:04:19 -0300

    agregamos un usuario comentario

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Modulos de Python en Coder House
 
 Estamos trabajando con repositorios con paquetes distribuibles y git-github
+
--- pruebGit.py
+++ pruebGit.py
@@ -1,3 +1,7 @@
 """ estoy aprendiendo a utilizar git con python"""
 
 #estamos viendo muchas cosas en vsc y tambien git
+
+"""
+otro comentario...
+"""
